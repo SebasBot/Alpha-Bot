d9309d4709a2415a51ea57c3c6955c5c232de8ac
SebasBot <juan.gongora.201433@gmail.com>
2021-10-01 21:09:46 -0500

    add simple invite

---
+++ Source/ComandosSlash/Diversion/invite.js
@@ -0,0 +1,27 @@
+const {SlashCommandBuilder} = require('@discordjs/builders')
+module.exports={
+    data: new SlashCommandBuilder()
+    .setName('invitar')
+    .setDescription('Responderé a una de tus preguntas'),
+    async execute(interaccion){
+        const nickname  = interaccion.member.user.username
+
+        const objeto = {
+            color:"RANDOM",
+            author:{
+                name:`${nickname} Ola ¿Por que no me añades a tu server?`
+            },
+            description: "Esta es la URL/invitacion \n https://discord.com/api/oauth2/authorize?client_id=854753076664729600&permissions=8&scope=bot%20applications.commands",
+            image: {
+                url: "https://c.tenor.com/hyRFiIX7e1sAAAAC/gif-club-penguin-dance.gif"
+            }   
+
+
+        }
+        await interaccion.reply({embeds: [objeto], ephemeral: true}) 
+
+
+    }
+    
+
+}
